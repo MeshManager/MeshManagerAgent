45a9d6dd5a043b184a13e4f6ce896e50910297d3
S6John <john_sungho@naver.com>
2025-06-05 10:35:16 +0900

    feat: add controller Logic

--- internal/controller/istioroute_controller.go
+++ internal/controller/istioroute_controller.go
@@ -19,12 +19,16 @@ package controller
 import (
 	"context"
 	"fmt"
+	istiov1beta1 "istio.io/client-go/pkg/apis/networking/v1alpha3"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	meshmanagerv1 "github.com/MeshManager/MeshManagerAgent.git/api/v1"
+
+	generator "github.com/MeshManager/MeshManagerAgent.git/internal/controller/generators"
 )
 
 // IstioRouteReconciler reconciles a IstioRoute object
@@ -47,14 +51,77 @@ type IstioRouteReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.0/pkg/reconcile
 func (r *IstioRouteReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
+
+	//나중에 아래 fmt.Print 지울 것
 	fmt.Print("yaml 변경됨")
+
+	var istioRoute meshmanagerv1.IstioRoute
+	if err := r.Get(ctx, req.NamespacedName, &istioRoute); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	_ = istioRoute.DeepCopy()
+
+	for _, svcConfig := range istioRoute.Spec.Services {
+		vs := generator.GenerateVirtualService(svcConfig)
+		if err := ctrl.SetControllerReference(&istioRoute, vs, r.Scheme); err != nil {
+			return ctrl.Result{}, err
+		}
+
+		if err := r.CreateOrUpdate(ctx, vs); err != nil {
+			logger.Error(err, "failed to manage VirtualService")
+			return ctrl.Result{}, err
+		}
+
+		dr := generator.GenerateDestinationRule(svcConfig)
+		if err := ctrl.SetControllerReference(&istioRoute, dr, r.Scheme); err != nil {
+			return ctrl.Result{}, err
+		}
+		if err := r.CreateOrUpdate(ctx, dr); err != nil {
+			logger.Error(err, "failed to manage DestinationRule")
+			return ctrl.Result{}, err
+		}
+
+		if svcConfig.Type == meshmanagerv1.StickyCanaryType {
+			ef := generator.GenerateEnvoyFilter(svcConfig)
+			if err := ctrl.SetControllerReference(&istioRoute, ef, r.Scheme); err != nil {
+				return ctrl.Result{}, err
+			}
+			if err := r.CreateOrUpdate(ctx, ef); err != nil {
+				logger.Error(err, "failed to manage EnvoyFilter")
+				return ctrl.Result{}, err
+			}
+		}
+
+	}
+
+	//TODO Update Status
+
 	return ctrl.Result{}, nil
+}
+
+func (r *IstioRouteReconciler) CreateOrUpdate(ctx context.Context, obj client.Object) error {
+	key := client.ObjectKeyFromObject(obj)
+	existing := obj.DeepCopyObject().(client.Object)
+
+	if err := r.Get(ctx, key, existing); err != nil {
+		if errors.IsNotFound(err) {
+			return r.Create(ctx, obj)
+		}
+		return err
+	}
+
+	obj.SetResourceVersion(existing.GetResourceVersion())
+	return r.Update(ctx, obj)
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *IstioRouteReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&meshmanagerv1.IstioRoute{}).
+		Owns(&istiov1beta1.VirtualService{}).
+		Owns(&istiov1beta1.DestinationRule{}).
+		Owns(&istiov1beta1.EnvoyFilter{}).
 		Complete(r)
 }
