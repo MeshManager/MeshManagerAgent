af558e775409b10a61485e451e577ff62f73be94
S6John <john_sungho@naver.com>
2025-06-10 16:00:43 +0900

    fix: rbac 수정2

--- internal/controller/istioroute_controller.go
+++ internal/controller/istioroute_controller.go
@@ -40,10 +40,6 @@ type IstioRouteReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=mesh-manager.meshmanager.com,resources=istioroutes,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=mesh-manager.meshmanager.com,resources=istioroutes/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=mesh-manager.meshmanager.com,resources=istioroutes/finalizers,verbs=update
-
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
