3fad0756c5b17fa2bcc4751727ecc115d2aa3495
S6John <john_sungho@naver.com>
2025-06-11 18:06:11 +0900

    fix: validation 제거
     []string에는 Pattern 형식으로 validation 불가

--- api/v1/istioroute_types.go
+++ api/v1/istioroute_types.go
@@ -87,7 +87,6 @@ type DarknessRelease struct {
 
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:MaxItems=10
-	// +kubebuilder:validation:Pattern=`^(?:\d{1,3}\.){3}\d{1,3}$`
 	IPs []string `json:"ips,omitempty"`
 }
 
