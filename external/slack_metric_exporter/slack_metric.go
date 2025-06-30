64c04f6ac3d8e1a0da31fc2197a32b0b411fce4d
S6John <john_sungho@naver.com>
2025-06-30 11:36:47 +0900

    fix: 임시 로그

--- external/slack_metric_exporter/slack_metric.go
+++ external/slack_metric_exporter/slack_metric.go
@@ -2,12 +2,16 @@ package slack_metric_exporter
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/slack-go/slack"
 )
 
 func SendSlackMessage(token, channelID, message string) error {
 	api := slack.New(token)
+
+	log.Println(token, channelID)
+
 	// PostMessage의 첫 번째 인자는 채널 ID, 두 번째 이후는 메시지 옵션입니다.
 	_, _, err := api.PostMessage(
 		channelID,
